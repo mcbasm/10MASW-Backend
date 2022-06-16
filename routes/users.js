8c43b696ece08cdb7c274a191e58bbd68734d036
mcbasm <sebas-789@hotmail.com>
2022-06-16 11:24:06 -0500

    Encrytion User Passwords 1.0

--- routes/users.js
+++ routes/users.js
@@ -36,8 +36,43 @@ router.get('/:id', (req, res, next) => {
       res.json(result)
   })
 })
+// Obtener el listado de los registros paginados
+router.post('/paginated', async (req, res, next) => {
+  const {
+    page = 1, limit = 10
+  } = req.body
+
+  const items = await User.find({
+      status: true
+    }).select({
+      "name": 1,
+      "lastName": 1,
+      "email": 1,
+      "phone": 1,
+      "role": 1
+    }).populate('role')
+    .limit(limit * 1)
+    .skip((page - 1) * limit)
+    .exec();
+  const count = await User.countDocuments({
+    status: true
+  });
+
+  res.json({
+    items,
+    pagination: {
+      limit,
+      totalItems: count,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page
+    }
+  });
+})
 // Crear un registro
 router.post('/', (req, res, next) => {
+  // Obtener el valor de la contraseña y desencriptarlo del frontend para encriptarlo de nuevo en el backend
+  const passwordFEdecrypted = encryption.decryptFE(req.body.password)
+  req.body.password = encryption.encrypt(passwordFEdecrypted)
   User.create(req.body, (err, result) => {
     if (err) return next(err);
     else
@@ -46,11 +81,32 @@ router.post('/', (req, res, next) => {
 })
 // Editar un registro
 router.put('/:id', (req, res, next) => {
-  User.findByIdAndUpdate(req.params.id, req.body, (err, result) => {
-    if (err) return next(err);
-    else
-      res.json(result)
-  })
+  // Obtener el valor de la contraseña y desencriptarlo del frontend para encriptarlo de nuevo en el backend
+  if (req.body.password) {
+    const passwordFEdecrypted = encryption.decryptFE(req.body.password)
+    req.body.password = encryption.encrypt(passwordFEdecrypted)
+    User.findByIdAndUpdate(req.params.id, req.body, (err, result) => {
+      if (err) return next(err);
+      else
+        res.json(result)
+    })
+  } else {
+    // Si no hay cambio de password, editar solo los campos basicos
+    User.findByIdAndUpdate(req.params.id, {
+      $set: {
+        name: req.body.name,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+        role: req.body.role,
+      }
+    }, (err, result) => {
+      if (err) return next(err);
+      else
+        res.json(result)
+    })
+  }
+
 })
 // Eliminar un registro
 router.delete('/:id', (req, res, next) => {
--- security/encryption.js
+++ security/encryption.js
@@ -1,10 +1,16 @@
 const crypto = require('crypto')
+const CryptoJS = require("crypto-js");
 
-const algorithm = 'aes-256-cbc'
 let salt = 'f844b09ff50c'
+let tokenFE = '654asd87as1cd'
 
 exports.encrypt = (value) => {
     let encryptedData = crypto.pbkdf2Sync(value, salt,
         1000, 64, `sha512`).toString(`hex`);
     return encryptedData;
+}
+
+exports.decryptFE = (value) => {
+    let bytes = CryptoJS.AES.decrypt(value, tokenFE)
+    return bytes.toString(CryptoJS.enc.Utf8)
 }
